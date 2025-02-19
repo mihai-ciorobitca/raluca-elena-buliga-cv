95ff29e2bef01f1a4164266cd33912e5b2df07ff
mihai.ciorobitca <mihai.ciorobitca@outlook.com>
2025-02-19 07:51:29 +0200

    m

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index');
